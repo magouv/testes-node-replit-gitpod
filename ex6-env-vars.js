4db9a9a1116edbc11665b2e86b99c91a14308e05
repl.it user <replituser@example.com>
2021-02-24 13:42:14 +0000

    exemplo de vars. de ambiente

---
+++ ex6-env-vars.js
@@ -0,0 +1,10 @@
+// Conforme documento docs/estudo-vars-ambiente.docx,
+// crie as duas variáveis de ambiente no terminal.
+
+// Acessando todas as variáveis
+console.log("Todas as minhas variáveis de ambiente:")
+console.log(process.env)
+
+// Acessando uma variável específica
+console.log("\nAcessando uma variável específica:")
+console.log(process.env.SUA_VAR)
